e27f5fcb5ae22ab4fcded1b06097e71213abe39b
Mariusz D <mariuszdl@wp.pl>
2023-02-22 18:35:01 +0100

    Add Factory Pattern

---
+++ Factory/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class Circle : Shape
+    {
+        public override void Render()
+        {
+            Console.WriteLine("Rendering circle");
+        }
+    }
+}
---
+++ Factory/Program.cs
@@ -0,0 +1,11 @@
+﻿//Factory (Fabryka) - służy do tworzenia klas pewnego abstrakcyjnego typu (w tym przykładzie Shape)
+
+using Factory;
+
+ShapeFactory shapeFactory= new ShapeFactory();
+
+var circle = shapeFactory.CreateShape(ShapeType.Circle);
+circle.Render();
+
+var triangle = shapeFactory.CreateShape(ShapeType.Triangle);
+triangle.Render();
