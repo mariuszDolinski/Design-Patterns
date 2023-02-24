4b68e71f41ad6bc2c72c373b34ec251fe5536a81
Mariusz D <mariuszdl@wp.pl>
2023-02-24 17:09:22 +0100

    Added Singleton pattern

---
+++ Singleton/Configuration.cs
@@ -0,0 +1,29 @@
+﻿namespace Singleton
+{
+    internal class Configuration
+    {
+        private static Configuration _instance = null;
+        private static object _instanceLock = new object();
+        private Configuration() 
+        { 
+        }
+        public string StringProperty { get; set; }
+        public int IntProperty { get; set; }
+        public static Configuration GetInstance()
+        {
+            //używamy locka na wypadek gdyby dwa różne wątki działające niezależnie od siebie
+            //jednocześnie chciały stworzyć obiekt Configuration, wtedy mogłoby dojść do utworzenia
+            //dwóch obiektów i nie byłby to Singleton. Lock blokuje dostęp do kodu innym wątkom,
+            //gdy jeden go wywołuje. Daj nam to gwarancje, że tylko jeden wątek w tym samym czasie
+            //może wykonać ciało metody GetInstance
+            lock (_instanceLock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Configuration();
+                }
+            }
+            return _instance;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿// Singleton - wzorzec projektowy służący do tworzenia klasy, która może mieć tylko jedną instancję
+//             może posłużyć np. do tworzenia klas konfiguracyjnych, tóe przez cały czas trwania programu
+//             powinny mieć niezmienny stan. Łamie jednal zasady SOLID jednej odpowiedzialności i open-close
+
+using Singleton;
+
+var conf1 = Configuration.GetInstance();
+var conf2 = Configuration.GetInstance();
+
+if(ReferenceEquals(conf1, conf2))
+{
+    Console.WriteLine("Configuration is singleton");
+}
